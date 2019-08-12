ffa2813e83892df6a01903c4f72e02552255a01e
charlie <you@example.com>
2019-08-12 08:24:46 -0400

    fix bugs

--- imports/ui/containers/SearchPageContainer.js
+++ imports/ui/containers/SearchPageContainer.js
@@ -1,5 +1,5 @@
 import { withMessageContext } from "/imports/ui/helpers/MessageContext";
-import SearchPage from "/imports/ui/pages/SearchPage";
+// import ProfilePage from "/imports/ui/pages/ProfilePage";
 import { Meteor } from "meteor/meteor";
 import { withTracker } from "meteor/react-meteor-data";
 import DataSet from "/imports/api/dataSet/DataSet";
@@ -8,43 +8,25 @@ import { compose } from "recompose";
 import { generateFields } from "/imports/util/getDatabaseFields";
 import User from "/imports/api/users/User";
 import callWithPromise from "../../util/callWithPromise";
-
-
-let frontendSkills = generateFields("frontend");
-let backendSkills = generateFields("backend");
-let dataSkills = generateFields("data");
-
+import SearchPage from "../pages/SearchPage";
 
 
 export default compose(
   withRouter,
   withTracker(() => {
-    let found = false;
-    const dataSetsHandle = Meteor.subscribe("dataSets");
+    const datasetHandleSearch = Meteor.subscribe("dataSets");
     Meteor.subscribe("users");
-    let dataSet = DataSet.find(
-      { userId: Meteor.userId() }
-    ).fetch();
+    let frontendSkills = generateFields("frontend");
+    let backendSkills = generateFields("backend");
+    let dataSkills = generateFields("data");
 
-    // if(dataSet.length===0){
-    //   dataSet = createEmptyObject();
-    // }
-    // let dataSets = DataSet.find({}).fetch();
-    if (dataSet.length > 0) {
-      dataSet = dataSet[0];
-      let user = User.findOne({ _id: dataSet.userId });
-      dataSet["name"] = user.profile.firstName + " " + user.profile.lastName;
-      found = true;
-    }
     return {
-      dataSet,
-      found,
-      connected: Meteor.status().connected,
-      loading: !dataSetsHandle.ready(),
+      loading: !datasetHandleSearch.ready(),
       frontendSkills,
       backendSkills,
       dataSkills
     };
+
   }),
-  withMessageContext
-)(SearchPage);
+  withMessageContext)(SearchPage);
+
