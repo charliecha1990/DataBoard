9958543dcb195f8e08a20994ed27cd57356831ec
Sujay Neglur <sneglur@deloitte.ca>
2019-08-14 11:31:31 -0400

    Search V1

--- imports/ui/components/Profile/EnhancedTable.jsx
+++ imports/ui/components/Profile/EnhancedTable.jsx
@@ -10,7 +10,7 @@ import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import EnhancedTableToolbar from "./EnhancedTableToolbar";
 import Paper from "@material-ui/core/Paper";
-import {flattenData,createRows,mapDataNew} from "../../../util/getDatabaseFields";
+import { flattenData, createRows, mapDataNew } from "../../../util/getDatabaseFields";
 
 
 let counter = 0;
@@ -40,87 +40,6 @@ function getSorting(order, orderBy) {
   return order === "desc" ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
 }
 
-// EnhancedTableHead.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
-//   onRequestSort: PropTypes.func.isRequired,
-//   onSelectAllClick: PropTypes.func.isRequired,
-//   order: PropTypes.string.isRequired,
-//   orderBy: PropTypes.string.isRequired,
-//   rowCount: PropTypes.number.isRequired,
-// };
-
-const toolbarStyles = theme => ({
-  root: {
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-      theme.palette.type === 'light'
-          ? {
-            color: theme.palette.secondary.main,
-            backgroundColor: lighten(theme.palette.secondary.light, 0.85),
-          }
-          : {
-            color: theme.palette.text.primary,
-            backgroundColor: theme.palette.secondary.dark,
-          },
-  spacer: {
-    flex: '1 1 100%',
-  },
-  actions: {
-    color: theme.palette.text.secondary,
-    display: 'flex'
-  },
-  title: {
-    flex: '0 0 auto',
-  },
-});
-
-// let EnhancedTableToolbar = props => {
-//   const { numSelected, classes } = props;
-
-//   return (
-//     <Toolbar
-//       className={classNames(classes.root, {
-//         [classes.highlight]: numSelected > 0,
-//       })}
-//     >
-//       <div className={classes.title}>
-//         {numSelected > 0 ? (
-//           <Typography color="inherit" variant="subheading">
-//             {numSelected} selected
-//           </Typography>
-//         ) : (
-//           <Typography variant="subheading" id="tableTitle">
-//             Data Tracking
-//           </Typography>
-//         )}
-//       </div>
-//       <div className={classes.spacer} />
-//       <div className={classes.actions}>
-//         {numSelected > 0 ? (
-//           <Tooltip title="Delete">
-//             <IconButton aria-label="Delete">
-//               <DeleteIcon />
-//             </IconButton>
-//           </Tooltip>
-//         ) : (
-//           <Tooltip title="Filter list">
-//             <IconButton aria-label="Filter list">
-//               <FilterListIcon />
-//             </IconButton>
-//           </Tooltip>
-//         )}
-//       </div>
-//     </Toolbar>
-//   );
-// };
-
-// EnhancedTableToolbar.propTypes = {
-//   classes: PropTypes.object.isRequired,
-//   numSelected: PropTypes.number.isRequired,
-// };
-
-// EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
 
 
 const styles = theme => ({
@@ -211,7 +130,7 @@ class EnhancedTable extends React.Component {
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, newData.length - page * rowsPerPage);
     return (
       <Paper className={classes.root}>
-        <EnhancedTableToolbar numSelected={selected.length}/>
+        <EnhancedTableToolbar numSelected={selected.length} />
         <div className={classes.tableWrapper}>
           <Table className={classes.table} aria-labelledby="tableTitle">
             <EnhancedTableHead
@@ -230,13 +149,13 @@ class EnhancedTable extends React.Component {
                 .map(n => {
                   // const isSelected = this.isSelected(n.id);
                   return (
-                    <DisplayRow key={n.id} row={n} align = {this.props.dataAlign}/>
+                    <DisplayRow key={n.id} row={n} align={this.props.dataAlign} />
 
                   );
                 })}
               {emptyRows > 0 && (
                 <TableRow style={{ height: 49 * emptyRows }}>
-                  <TableCell colSpan={6}/>
+                  <TableCell colSpan={6} />
                 </TableRow>
               )}
             </TableBody>
