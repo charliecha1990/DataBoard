2c9d21378721e3c333e9d9cd6d22e18af18da004
Sujay Neglur <sneglur@deloitte.ca>
2019-08-20 12:54:58 -0400

    Include mandatory subscribe to work

--- imports/ui/containers/SearchPageContainer.js
+++ imports/ui/containers/SearchPageContainer.js
@@ -11,17 +11,22 @@ export default compose(
   withRouter,
   withTracker(() => {
     const dataSetsHandle = Meteor.subscribe("dataSets");
+    const availabilityHandle = Meteor.subscribe("availability");
     Meteor.subscribe("users");
     let frontendSkills = generateFields("frontend");
     let backendSkills = generateFields("backend");
     let dataSkills = generateFields("data");
     let skillRange = ["0", "1", "2", "3", "4", "5"];
+    let skills = [...frontendSkills,...backendSkills,...dataSkills];
     // console.log(datasetHandleSearch)
     return {
       loading: !dataSetsHandle.ready(),
+      loadingAvail:!availabilityHandle.ready(),
       frontendSkills,
       backendSkills,
-      dataSkills
+      dataSkills,
+      skills,
+      skillRange
     };
 
   }),
