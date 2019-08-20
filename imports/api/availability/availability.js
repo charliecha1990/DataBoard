456d7b15df308b17c306fb5f812377ce84878409
Sujay Neglur <sneglur@deloitte.ca>
2019-08-20 12:52:25 -0400

    Availability Schema

--- imports/api/availability/availability.js
+++ imports/api/availability/availability.js
@@ -3,24 +3,24 @@ import { Class } from "meteor/jagi:astronomy";
 export const Availabilities = new Mongo.Collection("availability");
 
 const Availability = Class.create({
-    name: "Availability",
-    collection: Availabilities,
-    fields: {
-        email: {
-            type: String,
-            required: true
-        },
+  name: "Availability",
+  collection: Availabilities,
+  fields: {
+    email: {
+      type: String,
+      required: true
+    },
 
-        startDate: {
-            type: Date,
-            required: true
-        },
+    startDate: {
+      type: Date,
+      required: true
+    },
 
-        endDate: {
-            type: Date,
-            required: true
-        }
+    endDate: {
+      type: Date,
+      required: true
     }
-})
+  }
+});
 
 export default Availability;
